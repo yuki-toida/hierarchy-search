ab419e5653427c3190b20553275a949e88a6bf8e
Yuki Toida <yuki.toida.dev@gmail.com>
2016-11-27 22:33:24 +0900

    update

--- main.go
+++ main.go
@@ -20,16 +20,16 @@ const (
 )
 
 // Hierarchy 階層を表す
-type hierarchy int16
+type hierarchy int8
 
-// HierarchyInfo 階層情報を表す
-type hierarchyInfo struct {
+// capacity 階層の容量を表す
+type capacity struct {
 	Size  int64
 	Count int
 }
 
 // 階層マップ
-var hierarchyMap = map[string]hierarchyInfo{}
+var hierarchyMap = map[string]capacity{}
 
 // エントリポイント
 func main() {
@@ -39,40 +39,25 @@ func main() {
 	output()
 
 	end := time.Now()
-	fmt.Printf("%f(s))", (end.Sub(start)).Seconds())
+	fmt.Printf("%f(s)", (end.Sub(start)).Seconds())
 }
 
 // csvを出力します
 func output() {
 	var csv string
 	for k, v := range hierarchyMap {
-		//csv += fmt.Sprintf(k+",%v,%v\n", v.Size, v.Count)
 		csv += rootPath + k + "," + strconv.FormatInt(v.Size, 10) + "," + strconv.Itoa(v.Count) + "\n"
 	}
 
 	currentDir, _ := os.Getwd()
-	err := ioutil.WriteFile(currentDir+"/hierarchy.csv", []byte(csv), os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	// currentDir, _ := os.Getwd()
-	// file, _ := os.Create(currentDir + "/hierarchy.csv")
-	// defer file.Close()
-	// w := bufio.NewWriter(file)
-	// w.WriteString(csv)
-	// w.Flush()
+	ioutil.WriteFile(currentDir+"/capacity.csv", []byte(csv), os.ModePerm)
 }
 
-// 階層を探索します
+// 階層探索します
 func search(targetPath string, dir1 string, dir2 string) {
-	files, err := ioutil.ReadDir(targetPath)
-
-	if err != nil {
-		panic(err)
-	}
-
 	hierarchy := getHierarchy(dir1, dir2)
+
+	files, _ := ioutil.ReadDir(targetPath)
 
 	for _, file := range files {
 		fileName := file.Name()
@@ -84,8 +69,7 @@ func search(targetPath string, dir1 string, dir2 string) {
 				dir2 = fileName
 			case Other:
 			}
-			nextPath := targetPath + "/" + fileName
-			search(nextPath, dir1, dir2)
+			search(targetPath+"/"+fileName, dir1, dir2)
 		} else {
 			switch hierarchy {
 			case Root:
@@ -103,25 +87,21 @@ func search(targetPath string, dir1 string, dir2 string) {
 // 階層を取得します
 func getHierarchy(dir1 string, dir2 string) hierarchy {
 	if dir1 == "" {
-		// Root階層
 		return Root
 	} else if dir2 == "" {
-		// 第一階層
 		return First
 	} else {
-		// 第二階層以下
 		return Other
 	}
 }
 
-// 階層マップを更新します
+// 階層容量情報を更新します
 func updateHierarchyMap(key string, size int64) {
-	info, ok := hierarchyMap[key]
-	if ok {
-		info.Count++
-		info.Size += size
-		hierarchyMap[key] = info
+	if value, ok := hierarchyMap[key]; ok {
+		value.Count++
+		value.Size += size
+		hierarchyMap[key] = value
 	} else {
-		hierarchyMap[key] = hierarchyInfo{Size: size, Count: 1}
+		hierarchyMap[key] = capacity{Size: size, Count: 1}
 	}
 }
