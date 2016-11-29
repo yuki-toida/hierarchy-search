228497c5c9f7e1e490a144ae87e4f1c054763865
y.toida <y.toida@gloops.com>
2016-11-29 22:47:48 +0900

    update

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
 	"time"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 // ルートディレクトリパス
-//const rootPath = "//gfs/Shares/00_全社共有/01_セキュリティ対策ソフト/"
-const rootPath = "C:/Workspace/golang/"
-
-// 階層を表す
-const (
-	Root hierarchy = iota
-	First
-	Other
-)
-
-// Hierarchy 階層を表す
-type hierarchy int8
+const rootPath = "//gfs/Shares/00_全社共有/"
 
 // capacity 階層の容量を表す
 type capacity struct {
@@ -28,80 +21,93 @@ type capacity struct {
 	Count int
 }
 
-// 階層マップ
-var hierarchyMap = map[string]capacity{}
+// 階層容量マップ
+var capacityMap = map[string]capacity{}
 
 // エントリポイント
 func main() {
 	start := time.Now()
-
-	search(rootPath, "", "")
+	searchRoot()
 	output()
-
 	end := time.Now()
 	fmt.Printf("%f(s)", (end.Sub(start)).Seconds())
 }
 
 // csvを出力します
 func output() {
+	currentDir, _ := os.Getwd()
+	file, _ := os.OpenFile(currentDir+"/capacity.csv", os.O_CREATE|os.O_WRONLY, 0666)
+	defer file.Close()
+	writer := bufio.NewWriter(transform.NewWriter(file, japanese.ShiftJIS.NewEncoder()))
+
 	var csv string
-	for k, v := range hierarchyMap {
-		csv += rootPath + k + "," + strconv.FormatInt(v.Size, 10) + "," + strconv.Itoa(v.Count) + "\n"
+	for k, v := range capacityMap {
+		content := rootPath + k + "," + strconv.FormatInt(v.Size, 10) + "," + strconv.Itoa(v.Count) + "\n"
+		writer.WriteString(content)
+		csv += content
 	}
 
-	currentDir, _ := os.Getwd()
-	ioutil.WriteFile(currentDir+"/capacity.csv", []byte(csv), os.ModePerm)
+	fmt.Println(csv)
+	writer.Flush()
 }
 
 // 階層探索します
-func search(targetPath string, dir1 string, dir2 string) {
-	hierarchy := getHierarchy(dir1, dir2)
-
-	files, _ := ioutil.ReadDir(targetPath)
-
+func searchRoot() {
+	files, _ := ioutil.ReadDir(rootPath)
 	for _, file := range files {
-		fileName := file.Name()
 		if file.IsDir() {
-			switch hierarchy {
-			case Root:
-				dir1 = fileName
-			case First:
-				dir2 = fileName
-			case Other:
-			}
-			search(targetPath+"/"+fileName, dir1, dir2)
-		} else {
-			switch hierarchy {
-			case Root:
-				// TODO Rootのファイルの扱い
-			case First:
-				updateHierarchyMap(dir1, file.Size())
-			case Other:
-				updateHierarchyMap(dir1, file.Size())
-				updateHierarchyMap(dir1+"/"+dir2, file.Size())
-			}
+			fileName := file.Name()
+			fmt.Println(fileName)
+			capacityMap[fileName] = capacity{Size: 0, Count: 0}
+			searchFirst(fileName)
 		}
 	}
 }
 
-// 階層を取得します
-func getHierarchy(dir1 string, dir2 string) hierarchy {
-	if dir1 == "" {
-		return Root
-	} else if dir2 == "" {
-		return First
-	} else {
-		return Other
+func searchFirst(dir1 string) {
+	files, _ := ioutil.ReadDir(rootPath + "/" + dir1)
+	for _, file := range files {
+		if file.IsDir() {
+			fileName := file.Name()
+			fmt.Println(dir1 + "/" + fileName)
+			capacityMap[dir1+"/"+fileName] = capacity{Size: 0, Count: 0}
+			searchSecond(dir1, fileName)
+		} else {
+			updateMap(dir1, file.Size())
+		}
 	}
 }
 
-// 階層容量情報を更新します
-func updateHierarchyMap(key string, size int64) {
-	if value, ok := hierarchyMap[key]; ok {
-		value.Count++
-		value.Size += size
-		hierarchyMap[key] = value
-	} else {
-		hierarchyMap[key] = capacity{Size: size, Count: 1}
+func searchSecond(dir1 string, dir2 string) {
+	secondPath := rootPath + "/" + dir1 + "/" + dir2
+	files, _ := ioutil.ReadDir(secondPath)
+	for _, file := range files {
+		if file.IsDir() {
+			searchOther(dir1, dir2, secondPath+"/"+file.Name())
+		} else {
+			fileSize := file.Size()
+			updateMap(dir1, fileSize)
+			updateMap(dir1+"/"+dir2, fileSize)
+		}
 	}
+}
+
+func searchOther(dir1 string, dir2 string, targetPath string) {
+	files, _ := ioutil.ReadDir(targetPath)
+	for _, file := range files {
+		if file.IsDir() {
+			searchOther(dir1, dir2, targetPath+"/"+file.Name())
+		} else {
+			fileSize := file.Size()
+			updateMap(dir1, fileSize)
+			updateMap(dir1+"/"+dir2, fileSize)
+		}
+	}
+}
+
+func updateMap(key string, size int64) {
+	value, _ := capacityMap[key]
+	value.Count++
+	value.Size += size
+	capacityMap[key] = value
 }
