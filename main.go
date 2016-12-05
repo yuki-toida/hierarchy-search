e36e9d44645126761a14aeb5f384e8c8ea8d391f
y.toida <y.toida@gloops.com>
2016-12-05 22:18:20 +0900

    update

--- main.go
+++ main.go
@@ -13,24 +13,22 @@ import (
 )
 
 // ルートディレクトリパス
-const rootPath = "\\\\gfs\\Shares\\00_全社共有\\"
+const root = "\\\\gfs\\Shares\\00_全社共有\\"
 
-//const rootPath = "C:/Workspace/Es/svn/"
-
-// capacity 階層の容量を表す
-type capacity struct {
+// info 階層の容量を表す
+type info struct {
 	Size  int64
 	Count int
 }
 
 // 階層容量マップ
-var capacityMap = map[string]capacity{}
+var infoMap = map[string]info{}
 
 // エントリポイント
 func main() {
 	start := time.Now()
 
-	searchRoot()
+	search()
 	output()
 
 	end := time.Now()
@@ -39,90 +37,93 @@ func main() {
 
 // csvを出力します
 func output() {
-	currentDir, _ := os.Getwd()
-	file, _ := os.Create(currentDir + "/capacity.csv")
-	defer file.Close()
-	//writer := bufio.NewWriterSize(file, 20000)
-	writer := bufio.NewWriterSize(transform.NewWriter(file, japanese.ShiftJIS.NewEncoder()), 20000)
-	for k, v := range capacityMap {
-		writer.WriteString(rootPath + k + "," + strconv.FormatInt(v.Size, 10) + "," + strconv.Itoa(v.Count) + "\n")
+	dir, _ := os.Getwd()
+	f, _ := os.Create(dir + "/hierarchy-search.csv")
+	defer f.Close()
+	//writer := bufio.NewWriterSize(f, 20000)
+	w := bufio.NewWriterSize(transform.NewWriter(f, japanese.ShiftJIS.NewEncoder()), 20000)
+	for k, v := range infoMap {
+		w.WriteString(root + k + "," + strconv.FormatInt(v.Size, 10) + "," + strconv.Itoa(v.Count) + "\n")
 	}
-	writer.Flush()
-}
-
-func test() {
-	currentDir, _ := os.Getwd()
-	txt, _ := os.Open(currentDir + "/hoge.txt")
-	defer txt.Close()
-	scanner := bufio.NewScanner(txt)
-
-	file, _ := os.Create(currentDir + "/hoge.csv")
-	defer file.Close()
-	writer := bufio.NewWriterSize(transform.NewWriter(file, japanese.ShiftJIS.NewEncoder()), 20000)
-
-	for scanner.Scan() {
-		content := scanner.Text()
-		bytes, _ := writer.WriteString(content + "\n")
-		fmt.Println(bytes)
-	}
-	writer.Flush()
+	w.Flush()
 }
 
 // 階層探索します
-func searchRoot() {
-	files, _ := ioutil.ReadDir(rootPath)
-	for _, file := range files {
-		if file.IsDir() {
-			fileName := file.Name()
-			capacityMap[fileName] = capacity{Size: 0, Count: 0}
-			searchFirst(fileName)
+func search() {
+	fi, _ := ioutil.ReadDir(root)
+	for _, f := range fi {
+		if f.IsDir() {
+			name := f.Name()
+			infoMap[name] = info{Size: 0, Count: 0}
+			search1(name)
 		}
 	}
 }
 
-func searchFirst(dir1 string) {
-	files, _ := ioutil.ReadDir(rootPath + "/" + dir1)
-	for _, file := range files {
-		if file.IsDir() {
-			fileName := file.Name()
-			capacityMap[dir1+"/"+fileName] = capacity{Size: 0, Count: 0}
-			searchSecond(dir1, fileName)
+// 第一階層
+func search1(d1 string) {
+	fi, _ := ioutil.ReadDir(root + "/" + d1)
+	for _, f := range fi {
+		if f.IsDir() {
+			name := f.Name()
+			infoMap[d1+"/"+name] = info{Size: 0, Count: 0}
+			search2(d1, name)
 		} else {
-			updateMap(dir1, file.Size())
+			update(d1, f.Size())
 		}
 	}
 }
 
-func searchSecond(dir1 string, dir2 string) {
-	secondPath := rootPath + "/" + dir1 + "/" + dir2
-	files, _ := ioutil.ReadDir(secondPath)
-	for _, file := range files {
-		if file.IsDir() {
-			searchOther(dir1, dir2, secondPath+"/"+file.Name())
+// 第二階層
+func search2(d1 string, d2 string) {
+	path := root + "/" + d1 + "/" + d2
+	fi, _ := ioutil.ReadDir(path)
+	for _, f := range fi {
+		if f.IsDir() {
+			searchN(d1, d2, path+"/"+f.Name())
 		} else {
-			fileSize := file.Size()
-			updateMap(dir1, fileSize)
-			updateMap(dir1+"/"+dir2, fileSize)
+			size := f.Size()
+			update(d1, size)
+			update(d1+"/"+d2, size)
 		}
 	}
 }
 
-func searchOther(dir1 string, dir2 string, targetPath string) {
-	files, _ := ioutil.ReadDir(targetPath)
-	for _, file := range files {
-		if file.IsDir() {
-			searchOther(dir1, dir2, targetPath+"/"+file.Name())
+// その他階層
+func searchN(d1 string, d2 string, path string) {
+	fi, _ := ioutil.ReadDir(path)
+	for _, f := range fi {
+		if f.IsDir() {
+			searchN(d1, d2, path+"/"+f.Name())
 		} else {
-			fileSize := file.Size()
-			updateMap(dir1, fileSize)
-			updateMap(dir1+"/"+dir2, fileSize)
+			size := f.Size()
+			update(d1, size)
+			update(d1+"/"+d2, size)
 		}
 	}
 }
 
-func updateMap(key string, size int64) {
-	value, _ := capacityMap[key]
-	value.Count++
-	value.Size += size
-	capacityMap[key] = value
+func update(key string, size int64) {
+	v, _ := infoMap[key]
+	v.Count++
+	v.Size += size
+	infoMap[key] = v
 }
+
+// func test() {
+// 	currentDir, _ := os.Getwd()
+// 	txt, _ := os.Open(currentDir + "/hoge.txt")
+// 	defer txt.Close()
+// 	scanner := bufio.NewScanner(txt)
+
+// 	file, _ := os.Create(currentDir + "/hoge.csv")
+// 	defer file.Close()
+// 	writer := bufio.NewWriterSize(transform.NewWriter(file, japanese.ShiftJIS.NewEncoder()), 20000)
+
+// 	for scanner.Scan() {
+// 		content := scanner.Text()
+// 		bytes, _ := writer.WriteString(content + "\n")
+// 		fmt.Println(bytes)
+// 	}
+// 	writer.Flush()
+// }
